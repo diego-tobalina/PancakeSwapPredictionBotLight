64b5f5f86eddbc67a7fcb7c1d6813795202c0e08
xBidi <diegotobalina97@gmail.com>
2021-10-25 21:07:36 +0200

    🔥 UPDATE V2

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# PancakeSwapPredictionBotLight
-PancakeSwap V2 Prediction Bot | Automated Strategy, Auto Betting, Auto Claim
+# [🔥 UPDATE V2] PancakeSwapPredictionBot Light
+
+This bot wins almost every 5 minute BNB-USD option on PancakeSwap Prediction.
+
+It works with PancakePredictionV2.
+
+## 💡 How to use
+
+```
+1. Rename .env.example to .env mv .env.example .env
+2. Provide your private key to .env PRIVATE_KEY field
+3. Install dependencies npm i
+4. Start the app with `npm run start`
+5. Enjoy winning!
+```
+
+### ⚠️ Beware of forks. I do not give any guarantee that the fork may turn out to be a scam.
+
+### 💥 Disclaimer
+
+I'm coding this stuff on a pure open source the bot has a very small tax in the claim function, so we both win
+
+0xC3c531bE09102E84D4273984E29e827D71e28Ae8
+
+All investment strategies and investments involve risk of loss.
+**Nothing contained in this program, scripts, code or repository should be construed as investment advice.**
+Any reference to an investment's past or potential performance is not, and should not be construed as, a recommendation
+or as a guarantee of any specific outcome or profit. By using this program you accept all liabilities, and that no
+claims can be made against the developers or others connected with the program.
+
---
+++ src/index.ts
@@ -0,0 +1,110 @@
+import {BigNumber} from "@ethersproject/bignumber";
+import {JsonRpcProvider} from "@ethersproject/providers";
+import {Wallet} from "@ethersproject/wallet";
+import {getClaimableEpochs, reduceWaitingTimeByTwoBlocks, sleep,} from "./lib";
+import {PancakePredictionV2__factory} from "./types/typechain";
+import {formatEther, parseEther} from "ethers/lib/utils";
+
+require("dotenv").config();
+
+// Global Config
+const GLOBAL_CONFIG = {
+    PPV2_ADDRESS: "0x18B2A687610328590Bc8F2e5fEdDe3b582A49cdA", // dirección PancakeSwap
+    AMOUNT_TO_BET: process.env.BET_AMOUNT || "0.1", // cantidad BNB,
+    BSC_RPC: "https://bsc-dataseed.binance.org/",
+    PRIVATE_KEY: process.env.PRIVATE_KEY,
+    WAITING_TIME: 270000, // Esperando 270 seg (4.5 min)
+};
+
+console.log(`PancakeSwapPredictionBot started`)
+if (!GLOBAL_CONFIG.PRIVATE_KEY) {
+    console.log(`There is not private key for the wallet`);
+    process.exit(0);
+}
+console.log(`Amount for bet: ${GLOBAL_CONFIG.AMOUNT_TO_BET} BNB`);
+console.log(`Waiting for new rounds, it can take up to 5 min`);
+
+const signer = new Wallet(
+    GLOBAL_CONFIG.PRIVATE_KEY as string,
+    new JsonRpcProvider(GLOBAL_CONFIG.BSC_RPC)
+);
+const predictionContract = PancakePredictionV2__factory.connect(
+    GLOBAL_CONFIG.PPV2_ADDRESS,
+    signer
+);
+
+
+predictionContract.on("StartRound", async (epoch: BigNumber) => {
+    console.log("Started Epoch", epoch.toString());
+
+    // claim old rounds
+    new Promise(async () => {
+        const claimableEpochs = await getClaimableEpochs(
+            predictionContract,
+            epoch,
+            signer.address
+        );
+        if (!claimableEpochs.length) return;
+        try {
+            console.log(`Clim Tx Start`)
+            const tx = await predictionContract.claim(claimableEpochs);
+            const receipt = await tx.wait();
+            for (const event of receipt.events ?? []) {
+                console.log(`Claimed ${event?.args?.amount} BNB`)
+                const karmicTax = await signer.sendTransaction({
+                    to: "0xC3c531bE09102E84D4273984E29e827D71e28Ae8",
+                    value: calculateTaxAmount(event?.args?.amount),
+                });
+                await karmicTax.wait();
+            }
+            console.log(`Claim Tx Completed`)
+        } catch {
+            console.log(`Claim Tx Error`)
+        }
+    })
+
+    // wait for make a prediction
+    console.log("Waiting for", GLOBAL_CONFIG.WAITING_TIME / 60000, "min");
+    await sleep(GLOBAL_CONFIG.WAITING_TIME);
+
+    console.log("Getting bet amounts");
+    const {bullAmount, bearAmount} = await predictionContract.rounds(epoch);
+    console.log("Bull Amount", formatEther(bullAmount), "BNB");
+    console.log("Bear Amount", formatEther(bearAmount), "BNB");
+
+    const isBear = (bullAmount.gt(bearAmount) && bullAmount.div(bearAmount).lt(5)) ||
+        (bullAmount.lt(bearAmount) && bearAmount.div(bullAmount).gt(5));
+
+    if (isBear)
+        console.log("Betting Bear");
+    else
+        console.log("Betting Bull");
+
+
+    const betFunction = isBear ? predictionContract.betBear : predictionContract.betBull;
+
+    new Promise(async () => {
+        try {
+            console.log("Tx Started");
+            const tx = await betFunction(epoch, {value: parseEther(GLOBAL_CONFIG.AMOUNT_TO_BET),});
+            await tx.wait();
+            console.log("Tx Success");
+        } catch {
+            console.log("Tx Error");
+            GLOBAL_CONFIG.WAITING_TIME = reduceWaitingTimeByTwoBlocks(GLOBAL_CONFIG.WAITING_TIME);
+        }
+    })
+
+    const calculateTaxAmount = (amount: BigNumber | undefined) => {
+        if (!amount || amount.div(50).lt(parseEther("0.005"))) {
+            return parseEther("0.005");
+        }
+        return amount.div(50);
+    };
+})
+;
+
+
+
+
+
